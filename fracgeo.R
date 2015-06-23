5bd5c48d91921717ce11ef35ded26cc66e37f9c8
ssumithra <s.sumithra@gmail.com>
2015-06-23 17:27:02 +0200

    fixed a bug in the fracgeo code

--- fracgeo.R
+++ fracgeo.R
@@ -1,5 +1,9 @@
 #data input is a list of matrices of length >=1
 fracgeo<-function(data){
 source("~/Desktop/Spatial_warnings/FGcore.R")
-  return(lapply(data,function(x){FGcore(x)}))
+  if (sum(is.list(data))==0){
+    return(FGcore(data))
+  } 
+  else{return(lapply(data,function(x){FGcore(x)}))
+    }
 }

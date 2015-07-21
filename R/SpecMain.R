1264b16792ba98ea64911108c1d6ecf711d3fd16
Florian Schneider <florian.schneider@univ-montp2.fr>
2015-07-21 17:16:32 +0200

    cleanout powerspectrum function and rename to indicator_powerspectrum()

--- R/SpecMain.R
+++
@@ -1,9 +0,0 @@
-#data input is a list of matrices of length >=1
-SpecMain<-function(data){
-
-  if (sum(is.list(data))==0){
-    return(rspec_ews(data))
-  } 
-  else{return(lapply(data,function(x){rspec_ews(x)}))
-    }
-}

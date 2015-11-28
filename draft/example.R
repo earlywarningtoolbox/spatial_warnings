ed4c65d30d3cd1bd481c79b801b6a4831c4b53a9
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2015-11-28 19:48:04 +0530

    Added example interaction

--- draft/example.R
+++ draft/example.R
@@ -1,9 +1,44 @@
-# example
+# 
+# This file provides an example of use for the spatialwarning and the caspr
+# packages.
+# 
 
-# Data file having only one snapshot
-# data=read.table(file='desertification_SDF_R0.91.txt',header=FALSE,sep='\t');
 
-# Data file having many snapshots.
-data=read.table(file='desertification_BW.txt',header=FALSE,sep='\t');
+# Prerequisite: an installed version of spatialwarnings as user
+# 
+# This requires a working copy of devtools if installing from github.
+library(devtools)
+install_github('fdschneider/spatial_warnings')
 
-out=spatial_ews(data,discrete=FALSE,subsize=5,iter=20,nullmodel = 0)
+# We now have the package installed: we can load it
+library(spatialwarnings) # mind the absence of underscore ! 
+
+# An example dataset is provided with the package: forestdat
+# It is a list that contains simulations produced by the forestgap model. 
+# The list has two elements: matrices and parameters which are respectively
+# the 2D matrices of the last time step of each simulation and the parameters
+# used for each simulation. 
+# 
+# Let's make the matrices and parameters variables available to us
+data(forestdat)
+attach(forestdat) # take care to only run once
+
+# For example, we can have a look at the last landscape: 
+image(matrices[[10]]) 
+# ... and the parameters used for its simulation
+parameters[10, ]
+
+# Let's see what its psd look like: 
+psdfit <- indicator_fitpsd(matrices[[10]])
+plot(psdfit) # best fit is a power law 
+summary(psdfit) # with a slope of -0.8 +/- 0.1
+
+# Let's compute the generic EWS on the whole dataset and see whether it changes 
+# along the gradient (not only on the last mtx)
+generic_ic <- generic_spews(matrices)
+plot(generic_ic, along = parameters[ ,'delta']) # increases !
+
+generic_ic_summary <- summary(generic_ic) # can be slow
+generic_ic_summary
+plot(generic_ic_summary, along = parameters[ ,'delta'])
+

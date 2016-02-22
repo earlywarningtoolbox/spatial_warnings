6f11855600c8d41f0aba62424a0c749cbe852f4d
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2016-02-22 19:18:48 +0100

    Updated example to reflect change of functions

--- draft/example.R
+++ draft/example.R
@@ -1,6 +1,10 @@
 # 
-# This file provides an example of use for the spatialwarning package.
+# This file provides an example of use for the spatialwarning package. It will 
+# change and be adapted as the interaction with the spatialwarnings package
+# changes.
 # 
+
+library(lattice)
 
 
 # Prerequisite: an installed version of spatialwarnings as user
@@ -23,7 +27,7 @@ data(forestdat)
 attach(forestdat) # take care to only run once
 
 # For example, we can have a look at the last landscape: 
-image(matrices[[10]]) 
+levelplot(matrices[[10]]) 
 # ... and the parameters used for its simulation
 parameters[10, ]
 
@@ -37,9 +41,12 @@ summary(psdfit) # with a slope of -0.8 +/- 0.1
 generic_ic <- generic_spews(matrices)
 plot(generic_ic, along = parameters[ ,'delta']) # increases !
 
-generic_ic_summary <- summary(generic_ic) # can be slow
-generic_ic_summary
+# Now we can assess significance by comparing it to a reshuffled matrix of same
+# density, using the function indictest.
+generic_ic_summary <- indictest(generic_ic) # can be slow
+generic_ic_summary # this will print a pretty table
 plot(generic_ic_summary, along = parameters[ ,'delta'])
+
 
 
 # Second example with a real dataset
@@ -51,11 +58,46 @@ levelplot(desertification,
           col.regions = colorRampPalette(c('#FFF299', '#37A42C'))(20))
 
 # Classify into 0/1
-binary_mat <- as.binary_matrix( desertification > median(desertification) ) 
-levelplot(binary_mat, 
+desertification <- as.binary_matrix( desertification > median(desertification) ) 
+levelplot(desertification, 
           col.regions = colorRampPalette(c('#FFF299', '#37A42C'))(20))
 
 # Compute psd 
-psdfit <- indicator_fitpsd(binary_mat)
+psdfit <- indicator_fitpsd(desertification)
 summary(psdfit)
 plot(psdfit, all.models = TRUE)
+
+
+
+
+# Compute spectral spatial EWS
+# -----------------
+
+# On model data
+spectral_ic <- spectral_spews(matrices)
+# (Mind the warning !)
+
+# Plot the trend
+plot(spectral_ic)
+
+# Assess significance
+spectral_test <- indictest(spectral_ic)
+
+# Plot SDR trend
+plot(spectral_test)
+
+# Or plot spectrum trend
+plot_spectrum(spectral_test)
+
+
+# Do the same workflow on the real dataset
+desert_spectral_ic <- spectral_spews(desertification)
+desert_spectral_test <- indictest(desert_spectral_ic)
+
+# Plot spectrum
+plot_spectrum(desert_spectral_test)
+
+# This code produces an informative error as there is only one value 
+# of SDR available.
+plot(desert_spectral_test)
+

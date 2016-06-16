279aa03d207e04e59b5cf6483cd1e463cba289cf
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2016-06-16 16:58:32 +0200

    Optimize spectrum.cpp

--- src/rspectrum.cpp
+++ src/rspectrum.cpp
@@ -12,11 +12,13 @@ using namespace arma;
 // [[Rcpp::export]]
 DataFrame rspectrum(NumericMatrix rmat) { 
   
-  // Convert input matrix to mat
+  // Convert input matrix to arma-understandable stuff
   mat amat(rmat.begin(), 
            rmat.nrow(), rmat.ncol(), 
            false); // reuses memory and avoids extra copy
   
+  
+  // Get number of rows and number of cols
   int nr = amat.n_rows; 
   int nc = amat.n_cols; 
   
@@ -27,34 +29,59 @@ DataFrame rspectrum(NumericMatrix rmat) {
   // Minimum and maximum distances to consider
   int mi = 1;
   int ma = 1 + (n0x < n0y ? n0x : n0y);
+
+  // Initialize the variables that will hold the spectrum values
+  vec ray = linspace(mi, ma, ma-mi+1);
+  vec rspectr = zeros(ma-mi+1); 
+  
+  // We check whether there is more than one value in the supplied matrix
+  bool more_than_two_values = false;
+  int i=1;
+  while ( !more_than_two_values && i<amat.n_elem ) {
+    if ( amat(i-1) != amat(i) ) { 
+      more_than_two_values = true;
+    }
+    i++;
+  } 
+  
+  // We have not found more than two values -> return early
+  if ( !more_than_two_values ) { 
+    return DataFrame::create(_["dist"]  = ray, 
+                             _["rspec"] = NumericVector::create(NumericVector::get_na()));
+  }
   
   // Compute and shift DFT
   cx_mat mat_fft = fft2(amat); 
-  mat_fft = shift(mat_fft, n0x, 0); 
-  mat_fft = shift(mat_fft, n0y, 1); 
   
   // Compute aspectr2D and normalize it
   mat_fft(n0x, n0y) = 0; 
   
   // Compute r-spectrum
-  vec ray = linspace(mi, ma, ma-mi+1);
-  vec rspectr = zeros(ma-mi+1); 
   double step = 1;
   double norm_factor = 0; 
+  double aspectr2D_ij, dist;
+  int total_inmask, shift_i, shift_j;
   
   // For all lengths
   for (int l=0; l<ray.n_elem; l++) { 
     double r = ray(l);
     
     // Go through the distances matrix and make a sum of the relevant ones
-    double total_inmask = 0;
+    total_inmask = 0;
     for (int i=0; i<nr; i++) { 
       for (int j=0; j<nc; j++) { 
         
-        double dist = sqrt( pow(i - n0x, 2) + pow(j - n0y, 2) );
+        dist = sqrt( pow(i - n0x, 2) + pow(j - n0y, 2) );
         
         if ( dist >= r - step/2 && dist < r + step/2 ) { 
-          double aspectr2D_ij = pow(abs(mat_fft(i, j)), 2) / pow((n0x+1) * (n0y+1), 4);
+          
+          // We use shifted coordinates to pick the value in mat_fft 
+          // instead of making a call to arma::shift() that does a copy of 
+          // the matrix
+          shift_i = (i + n0x) % nr;
+          shift_j = (j + n0y) % nc;
+          aspectr2D_ij = pow(abs(mat_fft.at(shift_i, shift_j)), 2) / pow((n0x+1) * (n0y+1), 4);
+          
           rspectr(l) += aspectr2D_ij;
           norm_factor += aspectr2D_ij;
           total_inmask++;

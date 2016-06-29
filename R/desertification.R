c8a639d77cffe9996ee37bc3285120e402722cff
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2016-06-29 15:45:43 +0200

    Added arid dataset (images from Sonia)

--- R/desertification.R
+++
@@ -1,9 +0,0 @@
-#' A list of binary matrices and their associated parameters 
-#'
-#' @format A matrix 
-#' 
-#' @source An aerial image of a stressed arid ecosystem. 
-#' 
-#' @references TODO
-#' 
-"desertification"

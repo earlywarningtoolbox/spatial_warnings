f2e17eada93595cfe4e01587c12dfe25d382059a
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2016-06-29 17:43:30 +0200

    Cleaned a lot of old indicator functions.
    We can decide later to add them back in if we want to. The idea is to have a
    proper working package first, then add extras afterwards.

--- tests/testthat/test-indicators.R
+++ tests/testthat/test-indicators.R
@@ -4,12 +4,10 @@ context('Test that all indicator functions perform according to specification')
 test_that('indicator functions stop if provided garbage data', { 
 
   indicator_functions <- list(indicator_moran,
-                              indicator_cumpsd,
                               indicator_fracgeo,
                               indicator_largestpatch,
                               indicator_powerspectrum,
                               indicator_skewness,
-  #                             indicator_corrfunc, # Disabled to have the package build
                               indicator_variance) # add others here
   
   garbage_badclass  <- logical(10)

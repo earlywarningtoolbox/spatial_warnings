1ff5293aff3916cbb0e0ab0a49434a2616ee9b29
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2017-03-07 08:41:26 +0100

    Added some doc

---
+++ R/spatialwarnings.R
@@ -0,0 +1,13 @@
+# 
+# 
+# 
+# 
+#' @rdname spatiawarnings
+#' 
+#' @title Early Spatial-Warnings of Ecosystem Degradation
+#' 
+#' @name spatialwarnings
+#' 
+#' @aliases spatiawarnings-package
+#' 
+NULL

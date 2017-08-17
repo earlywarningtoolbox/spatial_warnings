8ad8557102530914b82818545cef1e195a451207
SabihaMajumder <sabiha.physics@gmail.com>
2017-08-17 10:39:00 +0530

    added details of serengeti

--- R/serengeti.R
+++ R/serengeti.R
@@ -6,8 +6,11 @@
 # 
 #' @title Serengeti dataset
 #' 
-#' @description A set of aerial photography taken along a rainfall gradient 
-#'   in Tanzania. 
+#' @description Vegetation data along a rainfall gradient in Serengeti national park. 
+#' The data-set consists of a recatangular area of size 7.5 km x 90 km. This data is represented as a list of matrices.
+#' Each matrix is a moving window of 7.5 km x 7.5 km which moves my 2.5 km. Each entry in the matrix is vegetation data classified into binary units with 0(grass) and 1(wood).
+#' 
+#' @details The resolution of vegetation data is 30m and that of rainfall data is 2.5km.    
 #' 
 #' @format A list of logical matrices 
 #' 
@@ -18,9 +21,13 @@
 #' 
 #' Eby, S., Agrawal, A., Majumder, S., Dobson, A.P. & Guttal, V. (2017). 
 #' Alternative stable states and spatial indicators of critical slowing down 
-#' along a spatial gradient in a savanna ecosystem: Eby et al. Global Ecology 
+#' along a spatial gradient in a savanna ecosystem: Global Ecology 
 #' and Biogeography, 26, 638-649
 
+#' Reed, D. N., Anderson, T. M., Dempewolf, J., Metzger, K., & Serneels, S. (2009). 
+#' The spatial distribution of vegetation types in the Serengeti ecosystem: 
+#' the influence of rainfall and topographic relief on vegetation patch characteristics. 
+#' Journal of Biogeography, 36(4), 770-782.
 # 
 # The strings below have no effect except forcing R to read this file. 
 "serengeti" 

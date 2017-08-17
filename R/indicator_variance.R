383da4273bdeee319c68d027749aa031b3b4b108
Vishwesha Guttal <vishwesha.guttal@gmail.com>
2017-08-17 10:11:18 +0530

    Update indicator_variance.R
    added @details

--- R/indicator_variance.R
+++ R/indicator_variance.R
@@ -4,9 +4,17 @@
 #'   It also computes a null value obtained by randomizing 
 #'   the matrix.
 #' 
-#' @references Guttal, V., and Jayaprakash, C. (2009). Spatial variance and 
+#' @references 
+#' Guttal, V., and Jayaprakash, C. (2009). Spatial variance and 
 #' spatial skewness: leading indicators of regime shifts in spatial 
 #' ecological systems. Theoretical Ecology, 2(1), 3-12.
+#' 
+#' Kefi, S., Guttal, V., Brock, W.A., Carpenter, S.R., Ellison, A.M., 
+#' Livina, V.N., et al. (2014). Early Warning Signals of Ecological 
+#' Transitions: Methods for Spatial Patterns. PLoS ONE, 9, e92097.
+#' 
+#' Sankaran, S., Majumder, S., Kefi, S., and Guttal, V. (2017). Implication of being discrete and spatial in detecting early warning signals
+#' of regime shifts
 #' 
 #' @param input A square matrix or a list of square matrices. The matrix entires can be binary, representing 0 (empty) or 1 (occupied). 
 #' The entries can also be continuous (like NDVI or EVI data). 
@@ -37,6 +45,22 @@
 #'                       in the null distribution.
 #'     }
 #' 
+#' @details
+#' Spatial variance is a measure of fluctuations in space. Based on the theory of critical slowing down, when systems approach critical points
+#' they are expected to show increased fluctuations in space. Thus, increasing spatial variance is proposed as an early warning signal of impending critical transitions. 
+#' 
+#' Computing spatial variance is straightforward. The function uses moments package to calcualte variance. However, detecting trends of variance that correspond to critical slowing down can be tricky, espeically if data come from discrete classification of state variable.
+#' For example, many high resolution spatial data are classified are 0 (empty) or 1 (occupied by plant). In such cases, spatial variance captures just the variance in data, but not that of spatial structure. 
+#' To resolve the issue, this function employs a method called coarse-graining, proposed in Kefi et al 2014 and described in detail in Sanakaran et al 2017. 
+#' Therefore, one must specify subsize for binary valued data sets. 
+#' 
+#' Input matrices can be either binary valued, or continuous data (numeric). 
+#'
+#' subsize has to be an integer. It has to be less than or equal to half of matrix size (N). subsize must also be preferably a divisor of N. 
+# If it is not a divisor of N, the remainde rows and columns are discarded when computing coarse-graining matrices. 
+#'
+#' Null model evaluations are also done on coarse-grained matrices. 
+#' 
 #' @examples
 #' 
 #' data(forestgap)
@@ -49,7 +73,7 @@ indicator_variance <- function(input,
                                detrending  = FALSE, 
                                nreplicates = 999) {
   
-  check_mat(input) # checks if binary and sensible
+  check_mat(input) # checks data input
   
   if (is.list(input)) { 
     # Returns a list of lists

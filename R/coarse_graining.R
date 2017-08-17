f626749f5c5bda87b0272d17df042760ebe00319
SabihaMajumder <sabiha.physics@gmail.com>
2017-08-17 10:03:53 +0530

    Added references to coarse_graining.R

--- R/coarse_graining.R
+++ R/coarse_graining.R
@@ -6,14 +6,17 @@
 
 #' @title Matrix coarse-graining
 #' 
-#' @description This function averages the spatial data locally. 
+#' @description This function averages the spatial data locally. This function divides the input matrix into submatrices of dimention subsize and averages the spatial data in these submatrices.
+#' By doing this, the dimention of resultant matrix is reduced by a factor of subsize. 
 
-#' @details This function divides the input matrix into submatrices of dimention subsize and averages the spatial data in these submatrices.
-#' By doing this, the dimention of resultant matrix is reduced by a factor of subsize.
-#' 
+#' @details If the data is classified into discrete units, the calculation of variance and autocorrelation can give spurious results irrelevant to the proximity to transition.
+#' Therefore, discrete data should be 'coarse-grained' before calculating the spatial early warning signals. However, this can also be applied to continuous state data.  
+
+#' @references Sankaran, S., Majumder, S., Kefi, S. and Guttal, V. (2017). Implications of being discrete and spatial for detecting early warning signals of regime shifts. Ecological Indicators. 
+
 #' @param mat A square matrix
 #' 
-#' @param subsize Dimetion of the submatrix. This has to be a positive integer smaller than the dimention of input matrix. 
+#' @param subsize Dimention of the submatrix. This has to be a positive integer smaller than the dimention of input matrix. 
 #' 
 #' @return A square matrix of reduced dimention.
 #'

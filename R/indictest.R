472b6838e835eb8a9ecf7791cfa46625a4367e6f
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2017-08-23 16:47:24 +0200

    Improved the global indictest() method doc

--- R/indictest.R
+++ R/indictest.R
@@ -1,5 +1,6 @@
 # 
-# Indictest is a method for testing the significance of spatial indicators. 
+# Indictest is a method for testing the significance of spatial indicators 
+# based on shuffling the original matrix. 
 # 
 # This file only defines the global method and maybe some helpers. For each 
 #   method, please refer to the task_* files. 
@@ -8,11 +9,11 @@
 #' 
 #' @description Assess the significance of spatial early-warning values
 #' 
-#' @param x A spatial warning object such as one produced by the *_spews 
+#' @param x A spatial warning object such as one produced by the \code{*_spews} 
 #'   function family
 #' 
-#' @param nperm How many times should the input matrix be randomized
-#'   to create the null distribution ?
+#' @param nperm The number of permutations to carry out to produce the null 
+#'   distribution 
 #' 
 #' @param ... Additional arguments passed to methods 
 #' 
@@ -34,9 +35,7 @@
 #'   Kefi, S., Guttal, V., Brock, W.A., Carpenter, S.R., Ellison, A.M., Livina, 
 #'   V.N., et al. (2014). Early Warning Signals of Ecological Transitions: 
 #'   Methods for Spatial Patterns. PLoS ONE, 9, e92097
-# 
-# 
-# Define global method
+#'  
 #'@export
 indictest <- function(x, nperm = 999, ...) { 
   UseMethod('indictest')

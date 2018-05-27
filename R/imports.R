57f6eaafa59d6aa55d68cb48f99e4bdf44994e83
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-05-27 11:01:07 +0200

    Pretend to import moments/poweRlaw

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,13 @@
-# 
-# These tags are here to explicitely show what is imported 
-# 
+#
+# These tags are here to explicitely show what is imported
+#
 #' @import stats
 #' @import utils
+
+# We need to pretend to import moments and poweRlaw as they
+# are used in the package build process. They are not
+# used in the final package tree though.
+# We import here two harmless functions so that CRAN
+# checks do note produce NOTEs (on certain archs only).
+#' @importFrom moments moment
+#' @importFrom poweRlaw get_n

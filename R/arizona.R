3573171bffcf67d3f505511eb6f9cf874f578c8a
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-10-09 14:35:41 +0200

    Added doc for arizona dataset

---
+++ R/arizona.R
@@ -0,0 +1,16 @@
+#' A list of classified aerial images of vegetation 
+#'
+#' @format A list of logical matrices which were obtained through the 
+#'   classification of aerial images of vegetation taken in Arizona (USA). 
+#' 
+#' @source Derived from the images provided in the Supplementary Material of 
+#'   Rodriguez et al. (2017). 
+#' 
+#' @references
+#' 
+#' Rodríguez, F., Á. G. Mayor, M. Rietkerk, and S. Bautista. 2017. A null model 
+#'   for assessing the cover-independent role of bare soil connectivity as 
+#'   indicator of dryland functioning and dynamics. Ecological Indicators.
+#'   
+"arizona"
+

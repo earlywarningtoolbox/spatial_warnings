9b5b7f01f9168f97c24a4f4a22e5e004e1f37866
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-11-05 19:52:36 +0100

    Improve treatment of Rodriguez et al.'s data

--- data-raw/rodriguez_et_al.R
+++ data-raw/rodriguez_et_al.R
@@ -3,6 +3,7 @@
 
 library(ggplot2)
 library(jpeg)
+library(EBImage) # Install from Bioconductor
 
 picdir <- "./data-raw/rodriguez_etal"
 pics <- dir(picdir, full = TRUE, pattern = "*.jpg")
@@ -24,7 +25,11 @@ imgs_bw <- lapply(imgs, summarise_pca)
 names(imgs_bw) <- names(imgs)
 
 # Compute indicators 
-imgs_classif <- lapply(imgs_bw, function(mat) mat > mean(mat))
+imgs_classif <- lapply(imgs_bw, function(mat) { 
+  clust <- mat > mean(mat)
+#   clust <- gblur(clust, sigma = 1) > .4 
+})
+lattice::levelplot(imgs_classif[[2]])
 
 psds <- patchdistr_spews(imgs_classif)
 

1dacdfe43b87361a0d9f56b517a2634fcd34da25
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-12-12 19:18:44 +0100

    Import doc additions from A.

--- R/task_flowlength.R
+++ R/task_flowlength.R
@@ -1,17 +1,36 @@
 # A function that computes the "simple" flowlength as described in Rodriguez 
 #   et al. 
 # 
-#' @title Flow length indicator
+#' @title Flowlength connectivity indicator (uniform topography)
 #' 
-#' @description Computes the flow length of a given matrix
+#' @description Measures the connectivity of runoff-source areas as determined 
+#'   by vegetation patterns and (uniform) topography
 #' 
 #' @details 
 #'   
-#'   This function computes the flow length of a given matrix, using a 
-#'     planar approximation (the slope is constant across the whole matrix), 
-#'     as per Rodriguez et al. (2017). 
-#'   
-#'   <Describe succintly (~100 wds) how this can be used as an indicator>
+#' This function computes Flowlength, a simple metric that measures the 
+#'   potential hydrological connectivity of runoff-source areas 
+#'   (e.g., bare soil) considering vegetation cover, vegetation patterns and 
+#'   topography. Flowlength is defined as the average length of all the 
+#'   potential runoff pathways in the target area. Thus, a higher value of 
+#'   the index indicates a higher hydrologic connectivity of runoff source 
+#'   areas. This function is designed for an idealized planar hillslope 
+#'   (e.g., with constant slope angle, the direction of maximum slope being 
+#'   from the top to the bottom of the input matrices). 
+#' 
+#' The deviations of Flowlength from its expected values under random or 
+#'   aggregated-pattern null models can be used as an indicator of imminent 
+#'   transition to a degraded state (Rodriguez et al. 2017) in the context 
+#'   of arid drylands. An increased deviation of flowlength compared to its 
+#'   null values is expected as a possible transition gets closer. 
+#' 
+#' In general, Flowlength can be used as indicator of dryland functional status 
+#'   by assessing potential water and soil losses in patchy landscapes 
+#'   (Mayor et al. 2008, Moreno-de las Heras et al. 2012, Mayor et al. 2013, 
+#'   Okin et al. 2015). Finally, the combination of observed and expected 
+#'   Flowlength under null models for random or aggregated vegetation cover 
+#'   can be used for assessing the cover-independent role of bare-
+#'   soil connectivity (Rodriguez et al. 2018).
 #'   
 #' @references
 #' 
@@ -19,7 +38,25 @@
 #'   for assessing the cover-independent role of bare soil connectivity as 
 #'   indicator of dryland functioning and dynamics. Ecological Indicators.
 #' 
-#' <Mayor et al. 2013 would probably be a good reference here too>
+#' Mayor, A.G., Bautista, S., Small, E.E., Dixon, M., Bellot, J., 2008. 
+#'   Measurement of the connectivity of runoff source areas as determined by 
+#'   vegetation pattern and topography: a tool for assessing potential water 
+#'   and soil losses in drylands. Water Resour. Res. 44, W10423.
+#' 
+#' Mayor, A.G., Kéfi, S., Bautista, S., Rodríguez, F., Cartení, F., Rietkerk, 
+#'   M., 2013. Feedbacks between vegetation pattern and resource loss 
+#'   dramatically decrease ecosystem resilience and restoration potential in 
+#'   a simple dryland model. Landsc. Ecol. 28, 931–942.
+#' 
+#' Moreno-de las Heras, M., Saco, P.M., Willgoose, G.R., Tongway, D.J., 2012. 
+#'   Variations in hydrological connectivity of Australian semiarid landscapes 
+#'   indicate abrupt changes in rainfall-use efficiency of vegetation. 
+#'   J. Geophys. Res. 117, G03009.
+#' 
+#' Okin, G.S., Moreno-de las Heras, M., Saco, P.M., Throop, H.L., Vivoni, E.R., 
+#'   Parsons, A.J., Wainwright, J., Peters, D.P.C., 2015. Connectivity in 
+#'   dryland landscapes: shifting concepts of spatial interactions. 
+#'   Front. Ecol. Environ. 13 (1), 20–27.
 #' 
 #' @param mat The input matrix (must be a logical matrix)
 #' 
@@ -89,8 +126,9 @@ flowlength_sews <- function(mat,
 #' 
 #' @details 
 #'   
-#'   This function computes the flow length of a given matrix, using a 
-#'     planar approximation (the slope is constant across the whole matrix), 
+#'   This function computes the Flowlength of a given matrix, using a 
+#'     planar approximation (the slope is constant across the whole matrix, 
+#'     with maximum slope being from the top of the matrix to its bottom), 
 #'     as per Rodriguez et al. (2017). See \code{\link{flowlength_sews}} for 
 #'     more details. 
 #'   

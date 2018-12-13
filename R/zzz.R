4f597d1b87f28ca17ed5734b05cd416fb4e2d3fc
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-12-14 00:20:05 +0100

    Disable the use of the global option spw.threads

--- R/zzz.R
+++ R/zzz.R
@@ -4,21 +4,15 @@
 # 
 # 
 .onAttach <- function(libname, pkgname){
-  if ( is.null( getOption("spw.threads") ) ) { 
-    options(spw.threads = 1)
-  }
   
   packageStartupMessage("This is spatialwarnings ", 
                         utils::packageDescription("spatialwarnings", 
                                                   fields = "Version"), 
                         appendLF = TRUE)
+
   if ( .Platform$OS.type == "unix" ) { 
     packageStartupMessage("Use options(mc.cores = <n>) to set up multi-core processing")
   }
-}
 
-
-.onUnload <- function(libpath) { 
-  options(spw.threads = NULL)
 }
 

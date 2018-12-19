de68181ce4d19b43e53952c941f171aa24e9cda5
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2018-12-20 00:04:43 +0100

    Fix accents in documentation and removing encoding in DESCRIPTION

--- R/arizona.R
+++ R/arizona.R
@@ -1,5 +1,5 @@
 #' A list of classified aerial images of vegetation 
-#'
+#' 
 #' @format A list of logical matrices which were obtained through the 
 #'   classification of aerial images of vegetation taken in Arizona (USA). 
 #' 
@@ -8,9 +8,8 @@
 #' 
 #' @references
 #' 
-#' Rodríguez, F., Á. G. Mayor, M. Rietkerk, and S. Bautista. 2017. A null model 
+#' Rodriguez, F., A. G. Mayor, M. Rietkerk, and S. Bautista. 2017. A null model 
 #'   for assessing the cover-independent role of bare soil connectivity as 
 #'   indicator of dryland functioning and dynamics. Ecological Indicators.
-#'   
+#' 
 "arizona"
-

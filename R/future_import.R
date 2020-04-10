2d3f5ac8aaee286b3569e7f3337ae16bf7b3f0ba
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2020-04-10 14:09:52 +0200

    Improve doc

--- R/future_import.R
+++
@@ -1,7 +0,0 @@
-# 
-# Spatialwarnings will load the `future` package at boot, but does not use 
-# directly its functions. Here we import plan() explicitely to make sure 
-# check() does not complain that we depend on a package we don't use. 
-#
-#'@importFrom future plan 
-NULL
--- R/roxygen-tags.R
+++ R/roxygen-tags.R
@@ -14,3 +14,24 @@
 # 
 # The NULL is mandatory so that devtools actually reads this file.
 NULL
+
+#
+# These tags are here to explicitely show what is imported
+#
+#' @import stats
+#' @import utils
+
+# We need to pretend to import moments and poweRlaw as they
+# are used in the package build process. They are not
+# used in the final package tree though.
+# We import here two harmless functions so that CRAN
+# checks do note produce NOTEs (on certain archs only).
+# See: https://groups.google.com/forum/#!topic/rdevtools/qT6cJt6DLJ0
+#' @importFrom moments moment
+#' @importFrom poweRlaw get_n
+
+# Spatialwarnings will load the `future` package at boot, but does not use 
+# directly its functions. Here we import plan() explicitely to make sure 
+# check() does not complain that we depend on a package we don't use. 
+#
+#'@importFrom future plan 

8bd40e730a6c0b1f04d8ed2cbf97040b598e962a
Alexandre Génin <alexandre.genin@univ-montp2.fr>
2020-09-07 13:13:03 +0200

    Make a test less stochastic

--- tests/testthat/test-variogram.R
+++ tests/testthat/test-variogram.R
@@ -14,18 +14,18 @@ test_that("Computation of variograms works", {
     for ( example_mat in mats ) { 
       example_mat = serengeti[[6]]
       nbins <- 32
-      nmax = 1e5L # prod(dim(example_mat)) ^2
+      nmax = 2e6L # 5e5L # prod(dim(example_mat)) ^2
       cutoff = sqrt(ncol(example_mat)^2 + nrow(example_mat)^2) / 10
       
       vario1 <- variogram_internal(example_mat, nmax, nbins, cutoff)
-      pairs(vario1)
+#       pairs(vario1)
       
       # Compare results to gstat
       locations <- expand.grid(seq.int(nrow(example_mat)),
                               seq.int(ncol(example_mat)))
       locations <- locations[sample.int(nrow(locations), 
                                         replace = FALSE, 
-                                        size = nmax/100), ]
+                                        size = min(nrow(locations),nmax/10)), ]
       values <- apply(locations, 1, function(X) example_mat[X[1], X[2]])
       locations.gstat <- sp::SpatialPointsDataFrame(locations, 
                                                     data.frame(z = values))
@@ -38,13 +38,14 @@ test_that("Computation of variograms works", {
       
       vario1_2 <- approx(vario1[ ,1], vario1[ ,2], vario2[ ,"dist"])$y
       
-      plot(vario1_2 - vario2[ ,"gamma"], vario2[ ,"gamma"])
-      abline(0,1)
+#       plot(vario1_2 - vario2[ ,"gamma"], vario2[ ,"gamma"])
+#       abline(0,1)
       
+      # The difference between the two variograms should be zero on average
       mod <- coef(lm(vario1_2 - vario2[ ,'gamma'] ~ 1))
       expect_true(abs(mod[1]) < 0.01) # Intercept ~= 0
       
-      # Test if P-value is significant, we shouls expect it not to be
+      # Test if P-value is significant, we expect it not to be
       expect_true(kruskal.test(vario1_2, vario2[ ,"gamma"])$p.value > 0.1)
     }
     
